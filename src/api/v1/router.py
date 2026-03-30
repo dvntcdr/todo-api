f7c089070b3dd9ce4032244a343c5a63352254b3
deviantcoder <andrewsafonovpost@gmail.com>
2026-03-30 11:20:15 +0300

    feat: add api v1 and auth routers

---
+++ src/api/v1/router.py
@@ -0,0 +1,8 @@
+from fastapi import APIRouter
+
+from .routes import auth
+
+
+v1_router = APIRouter(prefix='/v1')
+
+v1_router.include_router(auth.router)
--- src/main.py
+++ src/main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 
 from src import models  # noqa
 from src.core.config import settings
+from src.api.v1.router import v1_router
 
 
 app = FastAPI(**settings.fastapi_kwargs)
+
+app.include_router(v1_router)
