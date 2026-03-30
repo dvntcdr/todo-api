eb018709be6a4064fa0f6ffa7e8634213083814c
deviantcoder <andrewsafonovpost@gmail.com>
2026-03-30 13:02:00 +0300

    feat: add refresh token repo dependency to auth service

--- src/api/deps/auth.py
+++ src/api/deps/auth.py
@@ -3,11 +3,20 @@ from typing import Annotated
 from fastapi import Depends
 
 from src.api.deps.user import UserRepoDep
+from src.api.deps.session import SessionDep
 from src.services.auth import AuthService
+from src.repos.refresh_token import RefreshTokenRepository
 
 
-def get_auth_service(user_repo: UserRepoDep) -> AuthService:
-    return AuthService(user_repo)
+def get_refresh_token_repo(session: SessionDep) -> RefreshTokenRepository:
+    return RefreshTokenRepository(session)
+
+
+RefreshTokenRepoDep = Annotated[RefreshTokenRepository, Depends(get_refresh_token_repo)]
+
+
+def get_auth_service(user_repo: UserRepoDep, token_repo: RefreshTokenRepoDep) -> AuthService:
+    return AuthService(user_repo, token_repo)
 
 
 AuthServiceDep = Annotated[AuthService, Depends(get_auth_service)]
