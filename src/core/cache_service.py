b99e653bafa36b0038f9526a96120bd200707aba
deviantcoder <andrewsafonovpost@gmail.com>
2026-04-22 21:46:30 +0300

    refactor(cache): make service methods generic

--- src/core/cache_service.py
+++ src/core/cache_service.py
@@ -1,5 +1,4 @@
 import json
-from uuid import UUID
 
 from redis.asyncio import Redis
 
@@ -9,46 +8,21 @@ class CacheService:
     Redis cache service class
     """
 
-    USER_TTL: int = 300
-    MEMBERSHIP_TTL: int = 300
+    DEFAULT_TTL: int = 300
 
     def __init__(self, redis_client: Redis) -> None:
         self.client = redis_client
 
-    # USER
-
-    def _user_key(self, username: str) -> str:
-        return f'user:username:{username}'
-
-    async def get_user(self, username: str) -> dict | None:
-        data = await self.client.get(self._user_key(username))
-        return json.loads(data) if data else None
-
-    async def set_user(self, username: str, data: dict) -> None:
+    async def set(self, key: str, data: dict, ttl: int | None = None) -> None:
         await self.client.setex(
-            self._user_key(username),
-            self.USER_TTL,
-            json.dumps(data)
+            name=key,
+            time=ttl or self.DEFAULT_TTL,
+            value=json.dumps(data)
         )
 
-    async def invalidate_user(self, username: str) -> None:
-        await self.client.delete(self._user_key(username))
-
-    # MEMBERSHIP
-
-    def _membership_key(self, project_id: UUID, user_id: UUID) -> str:
-        return f'membership:{str(project_id)}:{str(user_id)}'
-
-    async def get_membership(self, project_id: UUID, user_id: UUID) -> dict | None:
-        data = await self.client.get(self._membership_key(project_id, user_id))
+    async def get(self, key: str) -> dict | None:
+        data = await self.client.get(key)
         return json.loads(data) if data else None
 
-    async def set_membership(self, project_id: UUID, user_id: UUID, data: dict) -> None:
-        await self.client.setex(
-            self._membership_key(project_id, user_id),
-            self.MEMBERSHIP_TTL,
-            json.dumps(data)
-        )
-
-    async def invalidate_membership(self, project_id: UUID, user_id: UUID) -> None:
-        await self.client.delete(self._membership_key(project_id, user_id))
+    async def invalidate(self, key: str) -> None:
+        await self.client.delete(key)
