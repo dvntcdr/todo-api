1e89b6646788e154881e356ae5bdf510757c73d0
deviantcoder <andrewsafonovpost@gmail.com>
2026-04-24 15:26:21 +0300

    feat(project): add cache dependency to project service dependency

--- src/api/deps/domain/project.py
+++ src/api/deps/domain/project.py
@@ -2,6 +2,7 @@ from typing import Annotated
 
 from fastapi import Depends
 
+from src.api.deps.cache import CacheServiceDep
 from src.api.deps.db.repos import MemberRepoDep, ProjectRepoDep
 from src.schemas.project import ProjectFilterParams
 from src.services.project import ProjectService
@@ -9,9 +10,10 @@ from src.services.project import ProjectService
 
 def get_project_service(
     project_repo: ProjectRepoDep,
-    member_repo: MemberRepoDep
+    member_repo: MemberRepoDep,
+    cache: CacheServiceDep
 ) -> ProjectService:
-    return ProjectService(project_repo, member_repo)
+    return ProjectService(project_repo, member_repo, cache)
 
 
 ProjectServiceDep = Annotated[ProjectService, Depends(get_project_service)]
