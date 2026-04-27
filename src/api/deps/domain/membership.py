7eecb6214577d846dee658af552bb2a3813d29d1
dvntcdr <dvntcdr@gmail.com>
2026-04-27 18:29:57 +0300

    fix(membership): add cache service dependency

--- src/api/deps/domain/membership.py
+++ src/api/deps/domain/membership.py
@@ -5,14 +5,16 @@ from fastapi import Depends
 from src.api.deps.domain.user import UserRepoDep
 from src.services.membership import ProjectMemberService
 from src.api.deps.db.repos import MemberRepoDep, ProjectRepoDep
+from src.api.deps.cache import CacheServiceDep
 
 
 def get_member_service(
     member_repo: MemberRepoDep,
     project_repo: ProjectRepoDep,
-    user_repo: UserRepoDep
+    user_repo: UserRepoDep,
+    cache: CacheServiceDep
 ) -> ProjectMemberService:
-    return ProjectMemberService(member_repo, project_repo, user_repo)
+    return ProjectMemberService(member_repo, project_repo, user_repo, cache)
 
 
 MemberServiceDep = Annotated[ProjectMemberService, Depends(get_member_service)]
