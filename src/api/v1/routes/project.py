1f085ca9fb6db6c29de6f166a109ae6713f824c7
dvntcdr <dvntcdr@gmail.com>
2026-05-06 14:12:52 +0300

    feat(project): add rate limiting

--- src/api/v1/routes/project.py
+++ src/api/v1/routes/project.py
@@ -1,10 +1,12 @@
 from uuid import UUID
 
 from fastapi import APIRouter, status
+from fastapi.requests import Request
 
 from src.api.deps.auth import CurrentUserDep
+from src.api.deps.domain.project import ProjectFiltersDep, ProjectServiceDep
 from src.api.deps.pagination import PaginationDep
-from src.api.deps.domain.project import ProjectServiceDep, ProjectFiltersDep
+from src.infra.rate_limit.limiter import limiter
 from src.models.project import Project
 from src.schemas.pagination import PagedResponse
 from src.schemas.project import ProjectCreate, ProjectResponse, ProjectUpdate
@@ -13,7 +15,9 @@ router = APIRouter(prefix='/projects', tags=['projects'])
 
 
 @router.get('/', response_model=PagedResponse[ProjectResponse])
+@limiter.limit('60/minute')
 async def get_projects(
+    request: Request,
     service: ProjectServiceDep,
     current_user: CurrentUserDep,
     pg_params: PaginationDep,
@@ -23,7 +27,9 @@ async def get_projects(
 
 
 @router.get('/{project_id}', response_model=ProjectResponse)
+@limiter.limit('100/minute')
 async def get_project(
+    request: Request,
     service: ProjectServiceDep,
     project_id: UUID,
     current_user: CurrentUserDep
@@ -32,7 +38,9 @@ async def get_project(
 
 
 @router.post('/', response_model=ProjectResponse, status_code=status.HTTP_201_CREATED)
+@limiter.limit('20/minute')
 async def create_project(
+    request: Request,
     service: ProjectServiceDep,
     data: ProjectCreate,
     current_user: CurrentUserDep
@@ -41,7 +49,9 @@ async def create_project(
 
 
 @router.patch('/{project_id}', response_model=ProjectResponse)
+@limiter.limit('30/minute')
 async def update_project(
+    request: Request,
     service: ProjectServiceDep,
     project_id: UUID,
     data: ProjectUpdate,
@@ -51,7 +61,9 @@ async def update_project(
 
 
 @router.delete('/{project_id}', status_code=status.HTTP_204_NO_CONTENT)
+@limiter.limit('15/minute')
 async def delete_project(
+    request: Request,
     service: ProjectServiceDep,
     project_id: UUID,
     current_user: CurrentUserDep
