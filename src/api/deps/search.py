a0fa9ddfd68a3d1f2def96a4dd05057013dd5bf0
dvntcdr <dvntcdr@gmail.com>
2026-05-09 18:31:00 +0300

    feat(search): add user repo to search dependency

--- src/api/deps/search.py
+++ src/api/deps/search.py
@@ -6,13 +6,15 @@ from src.api.deps.domain.project import ProjectRepoDep
 from src.api.deps.domain.task import TaskRepoDep
 from src.services.search import SearchService
 from src.schemas.search import SearchRequest
+from src.api.deps.domain.user import UserRepoDep
 
 
 def get_search_service(
     task_repo: TaskRepoDep,
-    project_repo: ProjectRepoDep
+    project_repo: ProjectRepoDep,
+    user_repo: UserRepoDep
 ) -> SearchService:
-    return SearchService(task_repo, project_repo)
+    return SearchService(task_repo, project_repo, user_repo)
 
 
 SearchServiceDep = Annotated[SearchService, Depends(get_search_service)]
